fe5f75f39b7bc55ac9fdf1ad0f37b9034208f4eb
Abhishekkute234 <kuteabhishek2004@gmail.com>
2025-01-10 09:36:39 +0530

    minor

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# The-Digital-Scroll
+# The-Digital-Scroll![alt text](<Screenshot 2025-01-10 091953.png>)
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import menuLinks from "@/data/menu";
-import SocialSharing from "./SocialSharing";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 export default function Header() {
@@ -35,47 +34,6 @@ export default function Header() {
               <rect y="12" width="25" height="4" fill="black" />
             </svg>
           </SheetTrigger>
-          <SheetContent
-            side="top"
-            className="w-full pt-14"
-            aria-label="Menu Toggle"
-          >
-            <nav
-              className="flex flex-col flex-1 justify-end gap-6"
-              aria-labelledby="mobile-nav"
-            >
-              {menuLinks.map((menuItem, index) => (
-                <Link key={index} href={menuItem.href}>
-                  {menuItem.label}
-                </Link>
-              ))}
-              <svg
-                width="15"
-                height="1"
-                viewBox="0 0 15 1"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="15" height="1" fill="black" />
-              </svg>
-              <SocialSharing
-                links={[
-                  {
-                    href: "#",
-                    ariaLabel: "Visit our Instagram page",
-                    src: "/icons/ri_instagram-line.svg",
-                    alt: "Instagram logo",
-                  },
-                  {
-                    href: "#",
-                    ariaLabel: "Visit our Twitter page",
-                    src: "/icons/ri_twitter-fill.svg",
-                    alt: "Twitter logo",
-                  },
-                ]}
-              />
-            </nav>
-          </SheetContent>
         </Sheet>
         <nav
           className="flex-1 items-center justify-end gap-6 hidden md:flex"
@@ -95,34 +53,6 @@ export default function Header() {
           >
             <rect width="15" height="1" fill="black" />
           </svg>
-          <SocialSharing
-            links={[
-              {
-                href: "#",
-                ariaLabel: "Visit our Instagram page",
-                src: "/icons/ri_instagram-line.svg",
-                alt: "Instagram logo",
-              },
-              {
-                href: "#",
-                ariaLabel: "Visit our Twitter page",
-                src: "/icons/ri_twitter-fill.svg",
-                alt: "Twitter logo",
-              },
-              {
-                href: "#",
-                ariaLabel: "Visit our YouTube page",
-                src: "/icons/ri_youtube-fill.svg",
-                alt: "YouTube logo",
-              },
-              {
-                href: "#",
-                ariaLabel: "Visit our RSS feed",
-                src: "/icons/ri_rss-fill.svg",
-                alt: "RSS feed logo",
-              },
-            ]}
-          />
         </nav>
       </div>
       <hr className="border-black border-t-0 border mt-4" />
